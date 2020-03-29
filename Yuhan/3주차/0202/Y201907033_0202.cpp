f5525cc1c34f696b6c6b0098c925dd7cd29ec2d0
김혜인 <k_hn_n@naver.com>
2020-03-29 21:00:20 +0900

    3주차 programming 실습

---
+++ Yuhan/3주차/0202/Y201907033_0202.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+double area(int r); // 함수의 원형 선언
+
+double area(int r) {    // 함수 구현 
+    return 3.14 * r * r;    // 반지름 r
+}
+
+
+int main()
+{
+    int n = 3;
+    char c = '#';   // 문자를 입력하므로 '' 
+    std::cout << c << 5.5 << '-' << n << "Hello" << true << std::endl;
+    std::cout << "n + 5 = " << n + 5 << '\n';
+    std::cout << "면적은 " << area(n) << '\n' ; // 함수 area()의 리턴 값 출력
+    system("pause");
+
+}
---
+++ Yuhan/3주차/0203/Y201907033_0203.cpp
@@ -0,0 +1,30 @@
+﻿// 0203번 과제 : 너비와 높이를 계산하는 부분은 함수로 작성
+// 함수란 ? : 0202 에서 봤던 내용 참고
+// 넓이와 높이가 둘다 실수로 받아야함
+
+#include <iostream>
+using namespace std;    // std::를 생략하기 위해 선언
+
+double area(double width, double height);   // 함수 원형 선언
+double area(double width, double height) {  // 사각형의 넓이 출력
+    return width * height;  // 사각형의 넓이를 계산한 값을 리턴
+}
+
+int main()
+{
+    // 로컬변수는 맨 위에 선언하는 것이 좋음.
+    double width, height;   // 실수형으로 받기 위해 자료형을 double로 선언
+   
+    cout << "너비를 입력하세요 >> ";
+    cin >> width; // 키보드로부터 너비를 읽어 width 변수에 저장
+                  // 입력이기때문에 부등호 방향이 반대
+    // width 변수 값에 문자가 들어가게 되면 비정상적으로 프로그램이 종료가 됨
+    // 예외적인 부분은 나중에 처리함.
+    cout << "높이를 입력하세요 >> ";
+    cin >> height;  // 키보드로부터 너비를 읽어 width 변수에 저장
+
+    cout << "면적은 " << area(width, height) << '\n'; //면적을 출력하고 다음 줄로 넘어감
+    
+    system("pause");    // 콘솔 창 유지
+    // [F9] => 중단점을 이용하면 유용
+}
