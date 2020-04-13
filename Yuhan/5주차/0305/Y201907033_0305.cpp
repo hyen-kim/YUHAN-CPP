384cff695a900c3ecd9e64ac91f2c180e6ba313f
김혜인 <k_hn_n@naver.com>
2020-04-13 19:14:08 +0900

    서두에 초깃값 + 위임생성자

---
+++ Yuhan/5주차/0305/Y201907033_0305.cpp
@@ -0,0 +1,33 @@
+﻿// 예제 3-5 멤버변수의 초기화와 위임 생성자 활용
+// 다음 Point 클래스의 멤버 x, y를 생성자 서두에 초기값으로 초기화하고
+// 위임 생성자를 이용하여 재작성하여라.
+
+#include <iostream>
+using namespace std;
+
+// 클래스 선언부
+class Point {
+    int x, y;
+public:
+    Point();
+    Point(int a, int b);
+    void show() { cout << "(" << x << "," << y << ")" << endl; }
+};
+
+// 클래스 생성부
+Point::Point() : Point(0,0) {}  // 위임생성자
+
+Point::Point(int a, int b) {
+    x = a;
+    y = b;
+}
+
+
+int main()
+{
+    Point origin(10, 20);
+    origin.show();
+
+    system("pause");
+
+}
