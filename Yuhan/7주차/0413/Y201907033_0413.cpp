08a77f47235ba3f75b3e6af1687f4af3daf0182d
김혜인 <k_hn_n@naver.com>
2020-04-28 18:39:42 +0900

    문자열 입력받고 회전 시키기

---
+++ Yuhan/7주차/0413/Y201907033_0413.cpp
@@ -0,0 +1,31 @@
+﻿// 전광판에 글자 흘러가는 것 처럼
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cout << "문자열을 입력하세요" << endl;
+    getline(cin, s, '\n');  // 문자열 입력
+    system("cls");      // 콘솔창 지우기
+    int len = s.length(); // 문자열의 길이
+
+    for (int i = 0; i < 100; i++)
+    {
+        string first = s.substr(0, 2);      // 맨 앞의 문자 1개를 문자열로 분리
+        string sub = s.substr(2, len - 2);  // 나머지 문자들을 문자열로 분리
+
+        s = sub + first;   // 두 문자열을 연결하여 새로운 문자열로 만듬
+
+        cout << s << endl;
+        Sleep(300); 
+        system("cls");  // 콘솔창 지우기
+    }
+    system("pause");
+
+}
+
