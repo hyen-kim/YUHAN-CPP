9fefbe6cee93d74dfde02c4aba47a74e7ee12eae
김혜인 <k_hn_n@naver.com>
2020-05-11 19:43:11 +0900

    디폴트 매개 변수를 이용하여 중복 함수 간소화 연습

---
+++ Yuhan/9주차/0605/Y201907033_0605.cpp
@@ -0,0 +1,17 @@
+﻿// 예제 6-5(실습) 디폴트 매개 변수를 이용하여 중복 함수 간소화 연습
+#include <iostream>
+using namespace std;
+
+void fillLine(int n = 25, char c = '*') {	// n개의 c 문자를 한 라인에 출력
+	for (int i = 0; i < n; i++)
+	{
+		cout << c;
+	}
+	cout << endl;
+}
+int main()
+{
+	fillLine();
+	fillLine(10, '%');
+}
+
