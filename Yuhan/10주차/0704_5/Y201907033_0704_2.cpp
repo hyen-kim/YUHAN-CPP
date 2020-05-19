fb51f5d333a96b25044517870f90d78098f81939
김혜인 <k_hn_n@naver.com>
2020-05-19 15:52:57 +0900

    += 연산자 함수 코드

---
+++ Yuhan/10주차/0704_5/Y201907033_0704_2.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include "Power.h"
+using namespace std;
+
+int main()
+{
+	Power a(3, 5), b(4, 6), c, d, e;
+	c = a + b;	// 파워 객체 + 연산
+	a.show();	// 3, 5
+	b.show();	// 4, 6
+	c.show();
+	d = a - b;
+	d.show();
+
+	if (a == b) {
+		cout << "두 파워가 같다." << endl;
+	}
+	else {
+		cout << "두 파워가 같지 않다." << endl;
+	}
+
+	e = a += b;
+	e.show();
+}
