e893623c870b9fbefc74c1d72e66554e1c365e3e
김혜인 <k_hn_n@naver.com>
2020-05-19 15:37:11 +0900

    == 연산자

---
+++ Yuhan/10주차/0706/Y201907033_0704_2.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include "Power.h"
+using namespace std;
+
+int main()
+{
+	Power a(3, 5), b(4, 6), c, d;
+	c = a + b;	// 파워 객체 + 연산
+	a.show();	// 3, 5
+	b.show();	// 4, 6
+	c.show();
+	d = a - b;
+	d.show();
+
+	if (a == b) {
+		cout << "두 파워가 같다." << endl;
+	}
+	else {
+		cout << "두 파워가 같지 않다." << endl;
+	}
+}
