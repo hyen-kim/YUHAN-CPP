ffa10827d47ddc66e6c652b6eff723dc6c91fe61
김혜인 <k_hn_n@naver.com>
2020-05-19 16:48:05 +0900

    +연산자에 값을 더하기

---
+++ Yuhan/10주차/0704_6/Power.cpp
@@ -0,0 +1,49 @@
+#include "Power.h"
+#include <iostream>
+using namespace std;
+
+Power::Power(int kick, int punch) {	// 여기서는 매개변수 디폴트값을 줬더라도 표시하면 안됨
+	this->kick = kick;
+	this->punch = punch;
+}
+
+void Power::show() {
+	cout << "kick= " << kick << ',' << "punch= " << punch << endl;
+}
+
+Power Power::operator+ (Power& op2) {	// + 연산자 함수 코드
+	Power tmp;
+	tmp.kick = this->kick + op2.kick;
+	tmp.punch = this->punch + op2.punch;
+	return tmp;
+}
+
+Power Power::operator- (Power& op2) {	// + 연산자 함수 코드
+	Power tmp;
+	tmp.kick = this->kick - op2.kick;
+	tmp.punch = this->punch - op2.punch;
+	return tmp;
+}
+
+bool Power::operator== (Power& op2) {	// == 연산자 함수 코드
+	if (this->kick == op2.kick && this->punch == op2.punch) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+Power& Power::operator+= (Power& op2) {	// += 연산자 함수 코드
+	// 자기 자신을 리턴할거기 때문에 tmp가 필요 없음
+	kick = kick += op2.kick;
+	punch = punch += op2.punch;
+	return *this;
+}
+
+Power Power::operator+ (int op2) {
+	Power tmp;
+	tmp.kick = this->kick + op2;
+	tmp.punch = this->punch + op2;
+	return tmp;
+}
---
+++ Yuhan/10주차/0704_6/Y201907033_0704_2.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include "Power.h"
+using namespace std;
+
+int main()
+{
+	Power a(3, 5), b(4, 6), c, d, e, f;
+	c = a + b;	// 파워 객체 + 연산
+	a.show();	// 3, 5
+	b.show();	// 4, 6
+	c.show();
+	d = a - b;
+	d.show();
+
+	if (a == b) {
+		cout << "두 파워가 같다." << endl;
+	}
+	else {
+		cout << "두 파워가 같지 않다." << endl;
+	}
+
+	e = a += b;
+	e.show();
+
+	cout << "==============================\n";
+	f = a + 2;
+	a.show();
+	f.show();
+
+}
