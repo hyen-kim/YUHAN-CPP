bea03c6d777f7a8da1ffc7c2fb763ee08cd14222
김혜인 <k_hn_n@naver.com>
2020-06-09 00:55:36 +0900

    vector 소팅

---
+++ Yuhan/13주차/1013/Y201907033_1013.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{   
+	vector<int> v;			// 정수 벡터 생성
+
+	cout << "5개의 정수를 입력하세요 >> ";
+	for (int i = 0; i < 5; i++) {
+		int n;
+		cin >> n;			// cin을 통해 5개 입력 받음
+		v.push_back(n);		// 키보드에서 읽은 정수를 벡터에 삽입
+	}
+
+	// v.begin()에서 v.end() 사이의 값을 오름차순으로 정렬
+	// sort() 함수의 실행 결과 벡터 v의 원소 순서가 변경됨
+	sort(v.begin(), v.end());	// sort를 쓰기 위해서 #include <algorithm>
+
+	vector<int>::iterator it;	// 벡터 내의 원소를 탐색하는 iterator 변수 선언
+
+	for (it = v.begin(); it != v.end(); it++)	// 벡터 v의 모든 원소 출력
+	{
+		cout << *it << "  ";
+	}
+
+	cout << endl;
+
+	system("pause");
+}
+
