1673e19b18b87ccf20e679f9d15eefe5ee64e7f8
김혜인 <k_hn_n@naver.com>
2020-06-15 01:23:34 +0900

    auto로 람다식 저장 및 호출

---
+++ Yuhan/14주차/1016/Y201907033_1016.cpp
@@ -0,0 +1,17 @@
+﻿// auto로 람다식 저장 및 호출
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    auto love = [](string a, string b) {
+        cout << a << "보다 " << b << "가 좋아" << endl;
+    };
+
+    love("돈", "너");             // 람다식 호출
+    love("냉면", "만두");         // 람다식 호출
+
+    system("pause");
+}
+
