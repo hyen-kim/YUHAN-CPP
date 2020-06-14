611b15cde1136451ed60af2466e75f72a31a7fe6
김혜인 <k_hn_n@naver.com>
2020-06-14 21:04:49 +0900

    auto를 이용한 변수 선언

---
+++ Yuhan/14주차/1014/Y201907033_1014.cpp
@@ -0,0 +1,43 @@
+﻿// 예제 10-14 auto를 이용한 변수 선언
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int square(int x) { return x * x; }
+
+int main()
+{
+    // 기본 타입 선언에 auto 활용
+    auto c = 'a';   // c는 char 타입으로 결정
+    auto pi = 3.14; // pi은 double 타입으로 결정
+    auto ten = 10;  // ten은 int 타입으로 결정
+    auto* p = &ten; // 변수 p는 int* 타입으로 결정
+
+    cout << c << " " << pi << " " << ten << " " << *p << endl;
+
+    // 함수의 리턴 타입으로 추론
+    auto ret = square(3);   // square() 함수의 리턴 타입이 int 이므로 ret는 int로 결정
+    cout << *p << " " << ret << endl;
+    
+    vector<int> v = { 1, 2, 3, 4, 5 };  // 벡터 v에 5개의 원소 1, 2, 3, 4, 5 삽입
+    vector<int>::iterator it;
+
+    for (it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " "; // 1 2 3 4 5 출력
+    }
+    cout << endl;
+
+    // 템플릿에 auto를 사용하여 간소화
+    for (auto it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " "; // 1 2 3 4 5 출력
+    }
+    cout << endl;
+
+    // 위아래 코드는 같은 코드
+    
+    system("pause");
+
+}
