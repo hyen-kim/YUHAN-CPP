0b239b9b4ea21fa1e6f46e707107a1c5127bb2b3
김혜인 <k_hn_n@naver.com>
2020-06-15 02:40:45 +0900

    반지름이 r이 원의 면적으로 리턴하는 람다식

---
+++ Yuhan/14주차/1017/Y201907033_1017.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double pi = 3.14;
+
+    auto calc = [pi](int r) -> double {return pi * r * r; };
+    cout << "면적은 " << calc(3) << endl;
+
+    auto calc2 = [](int r, double pi1) -> double {return pi1 * r * r; };
+    cout << "면적은 " << calc2(3, pi) << endl;
+
+    system("pause");
+}
